1e9d3e67a3d64a58dc8640fd9a2506e0849e13dd
Nikhil <nikhilljatt@gmail.com>
2025-01-18 08:15:26 +0530

    Finally added login page

--- flutter projects/recipe_app/lib/main.dart
+++ flutter projects/recipe_app/lib/main.dart
@@ -1,7 +1,15 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_app/homepage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+bool isLoading=false;
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-void main() {
   runApp(const MyApp());
 }
 
@@ -10,6 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromARGB(246, 252, 255, 255),
@@ -17,8 +26,233 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color.fromARGB(246, 252, 255, 255),
         ),
       ),
-      home: const Homepage(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // If user is logged in, navigate to the HomePage.
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            return const Homepage(); // Your custom HomePage
+          } else {
+            return const SignInPage(); // Sign-in page for new users or logged-out users
+          }
+        },
+      ),
 
     );
   }
 }
+
+
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  Future<User?> _signInWithGoogle() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        setState(() {
+          _isLoading = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Sign-in cancelled by user.")),
+        );
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      return userCredential.user;
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error during sign-in: $e")),
+      );
+
+      return null;
+    }
+  }
+
+  Future<User?> _signInWithEmailPassword(String email, String password) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      return userCredential.user;
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error during email sign-in: $e")),
+      );
+
+      return null;
+    }
+  }
+
+  Future<void> _registerWithEmailPassword(String email, String password) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      setState(() {
+        _isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Registration successful!")),
+      );
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error during registration: $e")),
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator()
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () async {
+                              User? user = await _signInWithEmailPassword(
+                                _emailController.text.trim(),
+                                _passwordController.text.trim(),
+                              );
+                          
+                              if (user != null) {
+                                print('Signed in as ${user.email}');
+                                if (mounted) {
+                                  Navigator.pushReplacement(
+                                    
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const Homepage(),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                            child: const Text('Sign In'),
+                          ),
+                          const SizedBox(width: 20,),
+                          ElevatedButton(
+                        onPressed: () async {
+                          await _registerWithEmailPassword(
+                            _emailController.text.trim(),
+                            _passwordController.text.trim(),
+                          );
+                        },
+                        child: const Text('Register '),
+                      ),
+                        ],
+                      ),
+                      
+                      const SizedBox(height: 20),
+                      const Divider(),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          User? user = await _signInWithGoogle();
+                          if (user != null) {
+                            print('Signed in as ${user.displayName}');
+                            if (mounted) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Homepage(),
+                                ),
+                              );
+                            }
+                          }
+                        },
+                        child: const Text('Sign In with Google'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+}
+
